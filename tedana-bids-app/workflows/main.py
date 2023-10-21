af3af795adf2fe2f91addf40f821b72660bcee99
Taylor Salo <tsalo006@fiu.edu>
2023-10-21 15:32:18 -0400

    More.

---
+++ tedana-bids-app/workflows/main.py
@@ -0,0 +1,27 @@
+"""Base workflows for the tedana BIDS App."""
+
+
+def init_tedana_wf():
+    """Identify subjects in dataset."""
+    ...
+
+
+def init_single_subject_wf():
+    """Identify runs for a given subject."""
+    ...
+
+
+def init_denoise_run_wf():
+    """Run tedana on a single run.
+
+    Steps
+    -----
+    1.  A BIDS data grabber to collect the first echo from each run.
+    2.  A BIDS data grabber to grab the other echoes, brain mask, echo times,
+        and potentially confounds.
+    3.  Drop dummy volumes as requested.
+    4.  Run tedana.
+    5.  Reorganize tedana outputs into BIDS format.
+    6.  Generate nireports HTML report.
+    """
+    ...
